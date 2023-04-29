9c8b1a9aac96ad5ad84eb451866ec50bdae61b95
V-Shelar <shelarvinod755@gmail.com>
2023-04-29 18:52:24 +0530

    Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,173 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  Python,
+  MySQL,
+  Mui,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  Bootstrap,
+  AntDesign,
+  docker,
+  Octoedge,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "Python",
+    icon: Python,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Bootstrap",
+    icon: Bootstrap,
+  },
+  {
+    name: "AntDesign",
+    icon: AntDesign,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MySQL",
+    icon: MySQL,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Intern",
+    company_name: "OCTOEDGE Technologies Pvt Ltd",
+    icon: Octoedge,
+    iconBg: "#383E56",
+    date: "Nov 2021 - March 2022",
+    points: [
+      "Participating in software development projects.",
+      "Collaborating with team members",
+      "Demonstrating initiative and take on new Challenges",
+      "Participating in code reviews.",
+    ],
+  },
+  {
+    title: "Front End Developer",
+    company_name: "OCTOEDGE Technologies Pvt Ltd",
+    icon: Octoedge,
+    iconBg: "#E6DEDD",
+    date: "April 2022 - November 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "OCTOEDGE Technologies Pvt Ltd",
+    icon: Octoedge,
+    iconBg: "#383E56",
+    date: "Dec 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+ 
+];
+
+export { services, technologies, experiences, testimonials, projects };
